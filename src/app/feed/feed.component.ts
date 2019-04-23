90620a978c91cf0ca257e5dad898100ccbcb3d49
gongzhenmu <a362296048@gmail.com>
2019-04-23 00:54:48 -0400

    basic send message functionality done

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,OnChanges } from '@angular/core';
+import { ChatService } from '../services/chat.service';
+import { Observable } from 'rxjs/Observable';
+import { ChatMessage } from '../models/chat-message.model';
+import { AngularFireList } from '@angular/fire/database';
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.css']
 })
-export class FeedComponent implements OnInit {
-
-  constructor() { }
+export class FeedComponent implements OnInit,OnChanges {
+   feed: Observable<ChatMessage[]>;
+  constructor(private chat: ChatService) {}
 
   ngOnInit() {
+
+    this.feed = this.chat.getMessages().valueChanges();
+    console.log('feedwork');
+  }
+
+  ngOnChanges(){
+    this.feed = this.chat.getMessages().valueChanges();
   }
 
 }
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs'
+import { AuthService } from '../services/auth.service';
+import * as firebase from 'firebase/app';
+import { ChatMessage } from '../models/chat-message.model';
 
 
 
@@ -6,5 +12,80 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ChatService {
+
+  user: firebase.User;
+
+  chatMessages: AngularFireList<ChatMessage>;
+  chatMessage: ChatMessage;
+  userName: Observable<string>;
+
+
+
+ constructor(
+    private db: AngularFireDatabase,
+    private afAuth: AngularFireAuth
+    ) {
+        //this.afAuth.authState.subscribe(auth => {
+        //  if (auth !== undefined && auth !== null) {
+         //   this.user = auth;
+        //  }
+
+        //  this.getUser().subscribe(a => {
+        //    this.userName = a.displayName;
+        //  });
+        //});
+    }
+
+
+ getMessages(): AngularFireList<ChatMessage> {
+    return this.db.list('messages', ref => ref.orderByKey().limitToLast(25));
+
+  }
+
+
+ getUser() {
+    const userId = this.user.uid;
+    const path = `/users/${userId}`;
+    return this.db.object(path);
+ }
+
+
+  sendMessage(msg: string){
+    const timestamp = this.getTimeStamp();
+    const email = 'test@example.com';
+    this.chatMessages = this.getMessages();
+    this.chatMessages.push({
+      message: msg,
+      timeSent: timestamp,
+      //userName: this.userName,
+      userName: 'test:user',
+      email: email
+      
+
+    });
+
+    console.log('called');
+  }
+
+
+
+
+
+    getUsers() {
+        const path = '/users';
+        return this.db.list(path);
+      }
+
+    getTimeStamp() {
+      const now = new Date();
+      const date = now.getUTCFullYear() + '/' +
+                   (now.getUTCMonth() + 1) + '/' +
+                   now.getUTCDate();
+      const time = now.getUTCHours() + ':' +
+                   now.getUTCMinutes() + ':' +
+                   now.getUTCSeconds();
+
+      return (date + ' ' + time);
+    }
   
 }
